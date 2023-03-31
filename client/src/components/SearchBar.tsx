8f212e3d99c68c09b4d50d7e6d3dea74b6cfd587
suhaibkhan699 <khansuhaib699@gmail.com>
2023-03-31 01:19:20 +0100

    clear search

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,38 +1,62 @@
 import { useState } from "react";
+import { SVG_NAMESPACE } from "../utils/constants/generic";
 
 const SearchBar = () => {
   const [searchText, setSearchText] = useState("");
 
-  const onSearchClick = (e: any) => {
-    if (e.key === "Enter" || e.type === "click") {
-      console.log(searchText);
-    }
+  const onFormSubmit = (e: any) => {
+    e.preventDefault();
+    alert(searchText);
   };
 
   return (
     <div className="font-sans text-black  flex items-center justify-center w-[600px]">
-      <div className="border rounded overflow-hidden flex w-[100%]">
-        <input
-          type="text"
-          className="px-4 py-2 w-[100%]"
-          placeholder="Search..."
-          onChange={(e) => {
-            setSearchText(e.target.value);
-          }}
-        />
-        <button
-          className="flex items-center justify-center px-4 border-l  bg-white"
-          onClick={onSearchClick}
-        >
-          <svg
-            className="h-4 w-4 text-grey-dark"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+      <div className="border rounded overflow-hidden flex w-[100%] h-9">
+        <form onSubmit={onFormSubmit} className="flex w-[100%]">
+          <input
+            type="text"
+            className="px-4 py-2 w-[100%]"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <span
+            className="bg-white self-center p-2 cursor-pointer"
+            onClick={() => {
+              setSearchText("");
+            }}
           >
-            <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-          </svg>
-        </button>
+            <svg
+              xmlns={SVG_NAMESPACE}
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </span>
+          <button
+            className="flex items-center justify-center px-4 border-l  bg-white"
+            type="submit"
+          >
+            <svg
+              className="h-4 w-4 text-grey-dark"
+              fill="currentColor"
+              xmlns={SVG_NAMESPACE}
+              viewBox="0 0 24 24"
+            >
+              <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+            </svg>
+          </button>
+        </form>
       </div>
     </div>
   );
--- client/src/utils/constants/generic.ts
+++ client/src/utils/constants/generic.ts
@@ -1,1 +1,2 @@
 export const shimmer_card_unit = 6
+export const SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
