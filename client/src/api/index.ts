a8713b25f42394d2819b3ccdd1f437d4662820bd
suhaibkhan699 <khansuhaib699@gmail.com>
2023-04-19 15:02:48 +0100

    end point changed

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,5 @@
-const url = "http://localhost:9000/"
+const url = "http://ec2-13-42-40-31.eu-west-2.compute.amazonaws.com/"
+//http://ec2-13-42-40-31.eu-west-2.compute.amazonaws.com/pools/get-top-pools
 
 export const fetchCategories = () => fetch(url)
 
