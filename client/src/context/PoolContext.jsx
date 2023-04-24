73d655abdee0a11eb339937364c9c82779799c81
rajdesai5434 <35265220+5434@users.noreply.github.com>
2023-04-24 16:27:08 +0100

    first pass at deposit

--- client/src/context/PoolContext.jsx
+++ client/src/context/PoolContext.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { ethers } from "ethers"
-// import { contractAbi, contractAddress } from "../utils/constants";
+import { poolContractABI, poolContractAddress, pmcContractABI, pmcContractAddress} from "../utils/constants/contract";
 
 export const PoolContext = React.createContext()
 
@@ -9,6 +9,28 @@ const { ethereum } = window
 export const PoolProvider = ({ children }) => {
 
     const [currentAccount, setCurrentAccount] = useState("")
+
+    const createPoolContract = () => {
+      const provider = new ethers.providers.Web3Provider(ethereum); //new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gasPrice = provider.getGasPrice();
+      const erc20VerifierContract = new ethers.Contract(poolContractAddress, poolContractABI, signer);
+      return {
+        poolGasPrice:gasPrice,
+        poolContract:erc20VerifierContract
+      };
+    };
+
+    const createPMCContract = () => {
+      const provider = new ethers.providers.Web3Provider(ethereum); //new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gasPrice = provider.getGasPrice();
+      const erc20VerifierContract = new ethers.Contract(pmcContractAddress, pmcContractABI, signer);
+      return {
+        pmcGasPrice:gasPrice,
+        pmcContract:erc20VerifierContract
+      };
+    };
 
     const checkIfWalletIsConnected = async () => {
         try{
@@ -37,6 +59,29 @@ export const PoolProvider = ({ children }) => {
 
     const placeBet = async () => {
         console.log("placing bet...")
+        const { poolGasPrice, poolContract } = createPoolContract();
+        const { pmcGasPrice, pmcContract } = createPMCContract();
+
+        console.log(poolGasPrice.toString());
+        console.log(pmcGasPrice.toString());
+
+        //const estimation = await poolContract.estimateGas.transfer(currentAccount, 100);
+        //console.log(estimation);
+
+        //const balance = await poolContract.accountBalance(currentAccount,ethers.utils.formatBytes32String('CSK'))
+        //console.log(ethers.utils.formatEther(balance))
+
+        const approve = await pmcContract.approve(poolContract.address,100,{gasPrice:pmcGasPrice,gasLimit:pmcGasPrice})
+        approve.wait()
+        console.log(approve)
+        const deposit = await poolContract.depositTokens(ethers.utils.formatBytes32String('PMC'),ethers.utils.formatBytes32String('CSK'),100,{gasPrice:poolGasPrice,gasLimit:poolGasPrice});
+        deposit.wait()
+        console.log(deposit)
+        //const approve = await pmcContract.approve(poolContract.address,50)
+        //console.log(approve)
+        //const deposit = await poolContract.depositTokens(ethers.utils.formatBytes32String('PMC'),ethers.utils.formatBytes32String('CSK'),10);
+        //console.log(pmcContract);
+
         // try{
         //     if(!ethereum) return alert("Please install metamask")
         //     const electionContract = getEthereumContract()
---
+++ client/src/utils/constants/contract.ts
@@ -0,0 +1,8 @@
+import poolAbi from '../PoolContractAbi.json'
+import pmcAbi from '../PMCContractAbi.json'
+
+export const poolContractABI = poolAbi.abi
+export const poolContractAddress = '0xD9e68459eb84b604D6dFe7bE1C0079Fc0365C971'
+
+export const pmcContractABI = pmcAbi.abi
+export const pmcContractAddress = '0x4D0B57A5C74cEE33ea5BD6C8E01F617324C7cC52'
